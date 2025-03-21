5bad1d928b67c00b9f8d1d71ee8284a2106532ac
Riyad Murad <riyadmurad01@gmail.com>
2025-03-21 14:28:31 +0200

    test pipe

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -14,7 +14,7 @@ const Login = () => {
   return (
     <div className="login-container">
       <form className="login-form" onSubmit={handleSubmit}>
-        <h2>Login</h2>
+        <h2>Login 2</h2>
         <div className="input-group">
           <label>Email</label>
           <input
